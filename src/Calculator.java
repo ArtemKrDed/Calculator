75e1415b551e6327c434950e34ca66a6bcacc85f
akrik <goodnightczar@mail.ru>
2025-07-27 22:56:18 +0300

    Добавление валидации и постоянного запроса если введен не 0

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,10 +6,27 @@ public class Calculator { //класс как название?
 
         System.out.println("Калькулятор");
         System.out.println("Введите число:");
+        while (!scanner.hasNextDouble()) {
+            System.out.println("Нужно число");
+            scanner.next();
+        }
         double number1 = scanner.nextDouble(); // обЪявление переменной number1
         System.out.println("Введите операцию (+,-,/,*)");
-        char operation = scanner.next().charAt(0);// если без charAT то работать не будет , так как scanner next - string , а не char , charAT метод который берет 1 символ из строки и преобразует его в char
+        String input;
+        while (true) {
+            input = scanner.next();
+            if (input.matches("[+\\-*/]")) {
+                return input.charAt(0);
+
+            }
+            System.out.println("Ошибка ввода , нуджен один из представленных операторов");
+        }
+        //char operation = scanner.next().charAt(0);// если без charAT то работать не будет , так как scanner next - string , а не char , charAT метод который берет 1 символ из строки и преобразует его в char
         System.out.println("Введите второе чило:");
+        while (!scanner.hasNextDouble()) {
+            System.out.println("Нужно число");
+            scanner.next();
+        }
         double number2 = scanner.nextDouble();
         double  result;
         switch (operation) {
@@ -20,11 +37,14 @@ public class Calculator { //класс как название?
                 result = number1 - number2;
                 break;
             case '/':
-                if (number2 !=0) {
-                    result = number1 / number2;
-                } else {
-                    System.out.println("На ноль делить нельзя");
-                    return;
+                while (true) {
+                    if (number2 != 0) {
+                        result = number1 / number2;
+                        break;
+                    } else {
+                        System.out.println("На ноль делить нельзя");
+                        number2 = scanner.nextDouble();//return;
+                    }
                 }
                 break;
             case '*':
